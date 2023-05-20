9795c01ab2c4f22c305930f526d0e966ec8c7f97
organelle2014 <richiejune@yahoo.com>
2023-05-20 13:07:37 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "shell.h"
+/***
+*add: The program adds two integers
+* a is the first number to be added
+* b is the second number to be added
+* 
+* return: the result
+*/
+
+int addNum(int a, int b)
+{
+    return (a + b);
+ }
+
